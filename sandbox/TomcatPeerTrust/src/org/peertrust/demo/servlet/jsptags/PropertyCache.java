c7ded4a6a1c9611aba0c68c8f88f68945ea0a45b
token77 <token77>
2005-11-29 16:30:21 +0000

    *** empty log message ***

--- sandbox/TomcatPeerTrust/src/org/peertrust/demo/servlet/jsptags/PropertyCache.java
+++ sandbox/TomcatPeerTrust/src/org/peertrust/demo/servlet/jsptags/PropertyCache.java
@@ -12,9 +12,16 @@ import java.util.Hashtable;
  * @author pat_dev
  *
  */
-public class PropertyCache {
-	
+public class PropertyCache 
+{
+	/**
+	 * The hashtable which hold the properties
+	 */
 	Hashtable cache= new Hashtable();
+	
+	/**
+	 * The peertrust evaluator
+	 */
 	PTPropertyEvaluator ptEvaluator;
 	
 	public void cacheProperty(PTParamCreationSpecification creationSpec)
@@ -28,20 +35,34 @@ public class PropertyCache {
 		}
 		
 		try {
-			cache.put(creationSpec.getParamName(),creationSpec.computParamValue(ptEvaluator));
+			cache.put(	creationSpec.getParamName(),
+						creationSpec.computParamValue(ptEvaluator));
 		} catch (BadObjectStateException e) {
 			e.printStackTrace();throw new BadObjectStateException("could not put cache prop",e);
 		}
 	}
 
+	/**
+	 * 
+	 * @return returns the peertrust evaluator
+	 */
 	public PTPropertyEvaluator getPtEvaluator() {
 		return ptEvaluator;
 	}
 
+	/**
+	 * Sets the peertrust evaluator
+	 * @param ptEvaluator
+	 */
 	public void setPtEvaluator(PTPropertyEvaluator ptEvaluator) {
 		this.ptEvaluator = ptEvaluator;
 	}
 	
+	/**
+	 * Returns a property
+	 * @param parameterName
+	 * @return
+	 */
 	public String getParameter(String parameterName){
 		if(parameterName==null){
 			throw new NullPointerException("Parameter parameterName must not be null");
--- sandbox/TomcatPeerTrust/src/org/peertrust/demo/servlet/jsptags/TagsPeerTrustEvaluator.java
+++ sandbox/TomcatPeerTrust/src/org/peertrust/demo/servlet/jsptags/TagsPeerTrustEvaluator.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.peertrust.demo.servlet.jsptags;
 
 
@@ -13,22 +10,42 @@ import org.peertrust.demo.servlet.NegotiationObjects;
 import org.peertrust.net.Peer;
 
 /**
- * @author pat_dev
+ * TagsPeerTrustEvaluator is responsible for evaluating whether a
+ * property is hold or not
+ * 
+ * @author Patrice Congo (token77)
  *
  */
 public class TagsPeerTrustEvaluator implements PTPropertyEvaluator {
 	static final public String PEER_NAME_PLACE_HOLDER="Requester"; 
-	NegotiationObjectRepository negoObjects;
-	TrustClient trustClient;
-	HttpSession session;
+	
 	/**
-	 * 
+	 * Provide a quick access to the trust objects
+	 */
+	NegotiationObjectRepository negoObjects;
+	
+	/**
+	 * The trustclient used for negotiation
+	 */
+	TrustClient trustClient;
+	
+	/**
+	 * The current http session
+	 */
+	HttpSession session;
+	
+	/**
+	 *Default contsructor 
 	 */
 	public TagsPeerTrustEvaluator() {
 		super();
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * Customizes the property with the remote negotiating peer alias
+	 * and uses the trust client to evaluate it.
+	 * @return true if the property is hold, or false if not
+	 *  
 	 * @see org.peertrust.demo.servlet.jsptags.PTPropertyEvaluator#eval(java.lang.String)
 	 */
 	public boolean eval(String propertySpec) {
@@ -41,7 +58,14 @@ public class TagsPeerTrustEvaluator implements PTPropertyEvaluator {
 		}
 	}
 
-	public String buildQuery(String propertySpec){
+	/**
+	 * Customizes the property spec with the alias of the remote negotiating
+	 * peer.
+	 *   
+	 * @param propertySpec -- the uncustomized property specification.
+	 * @return
+	 */
+	private String buildQuery(String propertySpec){
 		if(session!=null){
 			Peer peer= negoObjects.getSessionPeer(session.getId());
 			String peerName= null;
@@ -54,15 +78,15 @@ public class TagsPeerTrustEvaluator implements PTPropertyEvaluator {
 		return propertySpec;
 	}
 	
-	/* (non-Javadoc)
+	/**
 	 * @see org.peertrust.demo.servlet.jsptags.PTPropertyEvaluator#init(javax.servlet.jsp.PageContext)
 	 */
 	public void init(PageContext context) {
 		negoObjects=
 			NegotiationObjects.createAndAddForAppContext(context.getServletConfig());
 		trustClient=negoObjects.getTrustClient();
-		trustClient.setTimeout(1000*10);
-		trustClient.setSleepInterval(1000);
+		//trustClient.setTimeout(1000*10);
+		//trustClient.setSleepInterval(1000);
 		session=context.getSession();
 	}
 
